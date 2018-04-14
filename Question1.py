823d0cae3f5af2661afefcc4780a8fd8ba9d54ac
Iwen Gresham <33108411+evanGresham@users.noreply.github.com>
2018-04-14 11:46:24 +1000

    added sin() inputs

--- Question1.py
+++ Question1.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from numpy import round
 import matplotlib.pyplot as plt
 import csv
+import math
 
 
 def read_dataset(filename="spiralsdataset.txt"):
@@ -18,7 +19,7 @@ def read_dataset(filename="spiralsdataset.txt"):
             splittedRow = currentRow[0].split(' ')
             # list cleaner
 
-            newRow = [];
+            newRow = []
 
             for i in range(len(splittedRow)):
                 if (not splittedRow[i] == ""):
@@ -45,20 +46,25 @@ def read_dataset(filename="spiralsdataset.txt"):
 
 
 x, y = read_dataset()
+
+for i in x:
+    i.append(math.sin(i[0]))
+    i.append(math.sin(i[1]))
+
 labels = {0: 'spiral2', 1: 'spiral1'}
 
 # Create data placeholders
 # x_ is a placeholder for the inputs to the neural network
-x_ = tf.placeholder(tf.float32, [None, 2])
+x_ = tf.placeholder(tf.float32, [None, 4])
 # y_ is a place holder for the output of the neural network
 y_ = tf.placeholder(tf.float32, [None, 1])
 #
 # printf(x_)
 nodesInH1 = 8
-nodesInH2 = 5
-nodesInH3 = 2
+nodesInH2 = 8
+nodesInH3 = 1
 # Create first layer weights
-layer_0_weights = tf.Variable(tf.random_normal([2, nodesInH1]))
+layer_0_weights = tf.Variable(tf.random_normal([4, nodesInH1]))
 layer_0_bias = tf.Variable(tf.random_normal([nodesInH1]))
 layer_0 = tf.nn.sigmoid(tf.add(tf.matmul(x_, layer_0_weights), layer_0_bias))
 
@@ -72,20 +78,20 @@ layer_2_weights = tf.Variable(tf.random_normal([nodesInH2, nodesInH3]))
 layer_2_bias = tf.Variable(tf.random_normal([nodesInH3]))
 layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, layer_2_weights), layer_2_bias))
 
-# Create third layer weights
-layer_3_weights = tf.Variable(tf.random_normal([nodesInH3, 1]))
-layer_3_bias = tf.Variable(tf.random_normal([1]))
-layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, layer_3_weights), layer_3_bias))
+# # Create third layer weights
+# layer_3_weights = tf.Variable(tf.random_normal([nodesInH3, 1]))
+# layer_3_bias = tf.Variable(tf.random_normal([1]))
+# layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, layer_3_weights), layer_3_bias))
 
-outputs = layer_3
+outputs = layer_2
 
 # Define error function
 cost = tf.reduce_mean(tf.losses.mean_squared_error(labels=y_, predictions=outputs))
 
 # Define optimizer and its task (minimise error function)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=.001).minimize(cost)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=.5).minimize(cost)
 
-N_EPOCHS = 5000
+N_EPOCHS = 30000
 
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
@@ -93,7 +99,7 @@ tf.global_variables_initializer().run()
 print('Training...')
 
 errors = []
-
+plt.figure(1)
 # Train
 for i in range(N_EPOCHS):
     _, error = sess.run([optimizer, cost], feed_dict={x_: x, y_: y})
@@ -103,63 +109,26 @@ for i in range(N_EPOCHS):
 
 plt.plot(errors)
 
-plt.show()
-
-# assign colours so we can see 2 distinct spirals, blue for spiral 2, red for spiral 1 because we ae barbarians who do things back to front
-# colours = {0: 'blue', 1: 'red'}
-#
-# colour_map = [colours[i] for i in x]  # map all outputs to a colour for plotting
-
-
 def get_col(lst, col):
     return [row[col] for row in lst]
-
-# def plotData(x, y, colour_map, title, xLabel, yLabel)
-#     current = 0
-#     while len(x) > current:
-#         newX = x[current]
-#         newY = y[current]
-#
-
-# def plot_iris_data(x, y, colour_map, cls, title, xlabel, ylabel):
-#     plt.clf()
-#     plt.figure(2, figsize=(10, 6))
-#
-#     plt.title(title)
-#     plt.xlabel(xlabel)
-#     plt.ylabel(ylabel)
-#
-#     # Plot the training points
-#     for x_, y_, colour, c in zip(x, y, colour_map, cls):
-#         plt.scatter(x_, y_, c=colour, label=labels[c])
-#
-#     handles, label = plt.gca().get_legend_handles_labels()
-#     by_label = dict(zip(label, handles))
-#     plt.legend(by_label.values(), by_label.keys())
-#
-# #     plt.show()
-# #
-# #
-# # plot_iris_data(get_col(x, 0), get_col(x, 1), colour_map, y, "Sepal data", "Sepal length", "Sepal width")
-# #
-# # plot_iris_data(get_col(x, 2), get_col(x, 3), colour_map, y, "Petal data", "Petal length", "Petal width")
-# Display predictions
 
 
 classifications = sess.run(outputs, feed_dict={x_: x})
 for input, target, prediction in zip(x, y, classifications):
     print("input", input, "target", target, "prediction", prediction)
 
-
-
+plt.figure(2)
+plt.title("predictions, red is spiral 1 and blue is spiral 2")
 for input, prediction in zip(x,classifications):
     if(prediction[0]<0.5):
         plt.scatter(input[0], input[1],c = "red" )
     else:
         plt.scatter(input[0], input[1], c = "blue")
 
-plt.show()
+# plt.show()
 
+plt.figure(3)
+plt.title("green points are predicted correctly")
 for input,target, prediction in zip(x,y,classifications):
     if(round(prediction[0]) == target):
         plt.scatter(input[0], input[1],c = "green" )
